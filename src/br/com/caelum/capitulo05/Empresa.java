8411309844d45e25354b956553aacccf181d3233
Fabián <fabian@ufpa.br>
2017-11-17 14:45:15 -0300

    Acrescentado o capítulo 05.

---
+++ src/br/com/caelum/capitulo05/Empresa.java
@@ -0,0 +1,46 @@
+package br.com.caelum.capitulo05;
+
+public class Empresa {
+	String nome;
+	Funcionario[] empregados;
+	String cnpj;
+	
+	void adiciona(Funcionario f) {
+		//boolean arrayCheio = true;
+		for (int i = 0; i < empregados.length; i++) {
+			if (empregados[i] == null) {
+				empregados[i] = f;
+				//arrayCheio = false;
+				break;
+			} if (empregados[empregados.length - 1] != null) {
+				System.out.println("Todas as vagas já foram preenchidas!\nPrecisará de realocação");
+//				Funcionario[] arrayMaior = new Funcionario[this.empregados.length + 1];
+//				for (int j = 0; j < arrayMaior.length; j++) {
+//					arrayMaior[this.empregados.length] = f;
+//					empregados = arrayMaior;
+//				}
+				
+				break;
+			}
+		}
+		
+	
+	}
+	
+	void mostraEmpregados() {
+		for (int i = 0; i < empregados.length; i++) {
+			empregados[i].mostra();
+		}
+	}
+	
+	boolean contem(Funcionario f) {
+		for (int i = 0; i < empregados.length; i++) {
+			if (empregados[i] == f) {
+				System.out.println(f.nome + " faz parte da empresa " + this.nome);
+				return true;
+			} 
+		}
+		System.out.println(f.nome+ " não faz parte da empresa " + this.nome);
+		return false;
+	}
+}
