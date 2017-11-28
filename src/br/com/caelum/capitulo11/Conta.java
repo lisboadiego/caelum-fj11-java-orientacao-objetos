936d244301f2002a91a23f7485c1f92d657961dd
Diego Lisbôa <daslisboa@gmail.com>
2017-11-28 19:42:16 -0300

    Exercicios e desafio do capitulo 14 concluidos. Tmabém foi alterado a classe Conta do capitulo 11.

--- src/br/com/caelum/capitulo11/Conta.java
+++ src/br/com/caelum/capitulo11/Conta.java
@@ -1,10 +1,37 @@
 package br.com.caelum.capitulo11;
 
 public abstract class Conta {
+	private int numero;
 	protected double saldo;
+	private String cliente;
 
 	public double getSaldo() {
 		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	public String toString() {
+		return "esse objeto é uma conta com saldo R$" + this.saldo;
+	}
+	
+	public boolean equals(Object obj) {
+		Conta outraConta = (Conta) obj;
+		return ((this.numero == outraConta.numero) && (this.cliente == outraConta.cliente));
 	}
 	
 	public void deposita(double valor) {
